48d59724f9a5b3d503942ce95bea547e2ece8698
Lucas Walter <wsacul@gmail.com>
2021-02-19 21:14:12 -0800

    Now showing the image properly, but still taking up a lot of cpu- 50% for 1000x1000x10Hz image

--- src/view_image.cpp
+++ src/view_image.cpp
@@ -5,18 +5,19 @@
 struct ViewImage {
   ViewImage() {
     SDL_Init(SDL_INIT_EVERYTHING);
-    const size_t width = 2000;
-    const size_t height = 1000;
     win_ = SDL_CreateWindow("view_image", 50, 50, width, height, SDL_WINDOW_SHOWN);
     if (!win_) {
       SDL_Quit();
     }
-    ren_ = SDL_CreateRenderer(win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // Disabling these two doesn't help performance
+    ren_ = SDL_CreateRenderer(win_, -1, 0);  // SDL_RENDERER_ACCELERATED);  // | SDL_RENDERER_PRESENTVSYNC);
     if (!ren_) {
       SDL_Quit();
     }
     surface_ = SDL_GetWindowSurface(win_);
 
+    // SDL_SetRenderDrawColor(ren_, 100, 50, 20, 255);
+    // SDL_RenderClear(ren_);
     {
       const auto t0 = ros::Time::now();
       buffer_ = SDL_CreateTexture(
@@ -28,7 +29,9 @@ struct ViewImage {
           height);
 
       // this is okay for speed, mabye
-      std::array<int, width * height> pixels;
+      // std::array<int, width * height> pixels;
+      std::vector<int> pixels;
+      pixels.resize(width * height);
       for (size_t i = 0; i < pixels.size(); ++i ) {
         pixels[i] = 0x442288ff;
       }
@@ -37,15 +40,10 @@ struct ViewImage {
       int pitch = width * sizeof(int);
       // SDL_LockTexture(buffer_, NULL, reinterpret_cast<void**>(&pixels_ptr), &pitch);
       // SDL_UnlockTexture(buffer_);
-      SDL_UpdateTexture(buffer_, NULL, pixels.data(), 4);
-
-      // SDL_SetRenderDrawColor(ren_, 100, 50, 20, 255);
-      // SDL_RenderClear(ren_);
+      SDL_UpdateTexture(buffer_, NULL, pixels.data(), pitch);
       SDL_RenderCopy(ren_, buffer_, NULL, NULL);
 
-      // SDL_UpdateWindowSurface(win_);
-
-#if 0
+/*
       // This is super slow
       const auto t0 = ros::Time::now();
       for (int y = 0; y < height; ++y) {
@@ -54,14 +52,15 @@ struct ViewImage {
           SDL_RenderDrawPoint(ren_, x, y);
         }
       }
-#endif
+*/
       const auto t1 = ros::Time::now();
       SDL_RenderPresent(ren_);
       ROS_INFO_STREAM((t1 - t0).toSec());
     }
 
-    nh_.subscribe("image", 2, &ViewImage::imageCallback, this);
-    timer_ = nh_.createTimer(ros::Duration(0.1), &ViewImage::update, this);
+    image_sub_ = nh_.subscribe<sensor_msgs::Image>("image_in", 2, &ViewImage::imageCallback, this);
+    // timer_ = nh_.createTimer(ros::Duration(0.1), &ViewImage::update, this);
+    ROS_INFO_STREAM("done init");
   }
 
   ~ViewImage() {
@@ -77,10 +76,66 @@ struct ViewImage {
   void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     msg_ = msg;
 
+    // this is still taking up too much cpu- maybe the vsync?
+    const auto t0 = ros::Time::now();
+    buffer_ = SDL_CreateTexture(
+        ren_,
+        SDL_PIXELFORMAT_BGRA8888,
+        // SDL_TEXTUREACCESS_STREAMING,
+        SDL_TEXTUREACCESS_STATIC,
+        msg->width,
+        msg->height);
+
+    // std::array<int, msg->width * msg->height> pixels;
+    std::vector<int> pixels;
+    pixels.resize(msg->width * msg->height);
+
+    for (size_t i = 0; i < pixels.size(); ++i ) {
+      size_t msg_ind = i * 3;
+      pixels[i] = (msg->data[msg_ind + 0] << 24 |
+                   msg->data[msg_ind + 1] << 16 |
+                   msg->data[msg_ind + 2] << 8 |
+                   0xff);
+    }
+    int* pixels_ptr = pixels.data();
+    int pitch = msg->width * sizeof(int);
+    // SDL_LockTexture(buffer_, NULL, reinterpret_cast<void**>(&pixels_ptr), &pitch);
+    // SDL_UnlockTexture(buffer_);
+    SDL_UpdateTexture(buffer_, NULL, pixels.data(), pitch);
+
+    // SDL_SetRenderDrawColor(ren_, 100, 50, 20, 255);
+    // SDL_RenderClear(ren_);
+    SDL_Rect dst_rect;
+    dst_rect.x = 0;
+    dst_rect.y = 0;
+    dst_rect.w = std::min(msg->width, width);
+    dst_rect.h = std::min(msg->height, height);
+    SDL_Rect src_rect = dst_rect;
+    SDL_RenderCopy(ren_, buffer_, &src_rect, &dst_rect);
+
+    // SDL_UpdateWindowSurface(win_);
+/*
+    // This is super slow 10x slower than above
+    const auto t0 = ros::Time::now();
+    for (int y = 0; y < height; ++y) {
+      for (int x = 0; x < width; ++x) {
+        SDL_SetRenderDrawColor(ren_, 255, x % 255, 0, 255);
+        SDL_RenderDrawPoint(ren_, x, y);
+      }
+    }
+*/
+    const auto t1 = ros::Time::now();
+    SDL_RenderPresent(ren_);
+    ROS_INFO_STREAM("image update " << (t1 - t0).toSec());
+    SDL_DestroyTexture(buffer_);
   }
 
   void update(const ros::TimerEvent& event) {
+    ROS_INFO_STREAM_THROTTLE(5.0, "update");
   }
+
+  const Uint32 width = 2000;
+  const Uint32 height = 1000;
 
   SDL_Window* win_ = nullptr;
   SDL_Renderer* ren_ = nullptr;
@@ -88,6 +143,7 @@ struct ViewImage {
   SDL_Texture* buffer_ = nullptr;
 
   ros::NodeHandle nh_;
+  ros::Subscriber image_sub_;
   sensor_msgs::ImageConstPtr msg_ = nullptr;
   ros::Timer timer_;
 };
