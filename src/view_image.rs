5eebd1e2f9560c8084bc8ba5a78e16a79c477290
Lucas Walter <wsacul@gmail.com>
2021-02-21 21:07:01 -0800

    Now using a class to store the image_subscriber, but still have to use a closure- that seems fine but still curious if passing an object method is possible.  Observed that a 10Hz 2000x1000 image from image_publisher took 15% cpu for rqt_image_view, but that doubles when one or more image_viewer_rs node is running- a copy is being forced to take place?  Probably because the Image has to be mut in the image_viewer_rs callback?  But the image_viewer_rs take 20%, so appears to be outperforming rqt_image_view.

--- src/view_image.rs
+++ src/view_image.rs
@@ -28,32 +28,71 @@ fn make_test_image(width: usize, height: usize) -> Image {
     }
     msg
 }
-struct ViewImage {
+struct ViewImageBase {
     _sdl_context: sdl2::Sdl,
     window: sdl2::video::Window,
     event_pump: sdl2::EventPump,
+    // txrx: (crossbeam_channel::Sender<Image>, crossbeam_channel::Receiver<Image>),
+    rx: crossbeam_channel::Receiver<Image>,
 }
+struct ViewImage {
+    base: ViewImageBase,
+    _image_sub: rosrust::Subscriber,
+}
+
+/*
+impl ViewImageBase {
+    fn image_callback(&mut self, msg: Image) {
+        let (tx, _) = self.txrx;
+        if let Err(err) = tx.send(msg) {
+            if rosrust::is_ok() {
+                eprintln!("{:?}", err);
+            }
+        }
+    }
+}
+*/
 
 impl ViewImage {
     fn default(width: usize, height: usize) -> Self {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
-
         let window = video_subsystem.window("sdl2_ros", width as u32, height as u32)
             .position_centered()
             .build()
             .unwrap();
         let event_pump = sdl_context.event_pump().unwrap();
 
-        Self {
+        let (tx, rx) = crossbeam_channel::unbounded::<Image>();
+
+        let base = ViewImageBase {
             _sdl_context: sdl_context,
             window,
             event_pump,
+            rx,
+        };
+
+        // TODO(lucasw) want this to the callback in ViewImageBase
+        let image_callback = {
+            move |msg: Image| {
+                if let Err(err) = tx.send(msg) {
+                    // an error on shutdown isn't interesting (though preventing those
+                    // in the future would be nice
+                    if rosrust::is_ok() {
+                        eprintln!("{:?}", err);
+                    }
+                }
+            }
+        };
+
+        let _image_sub = rosrust::subscribe("image_in", 4, image_callback).unwrap();
+        Self {
+            base,
+            _image_sub,
         }
     }
 
-    fn msg_to_surface(&mut self, msg: &mut Image)
-    {
+    fn msg_to_surface(&mut self, msg: &mut Image) {
         let surface = Surface::from_data_pixelmasks(
             msg.data.as_mut_slice(),
             msg.width,
@@ -68,15 +107,16 @@ impl ViewImage {
             },
         ).unwrap();
 
-        let mut win_surface = self.window.surface(&self.event_pump).unwrap();
-        // TODO(lucas) define a destination sdl2::rect::Rect that preserves aspect ratio
+        let mut win_surface = self.base.window.surface(&self.base.event_pump).unwrap();
+        // TODO(lucas) define a destination sdl2::rect::Rect that
+        // centers and preserves aspect ratio
         //     sdl2::rect::Rect::new(0, 0, 300, 500),
         surface.blit(None, &mut win_surface, None).unwrap();
         win_surface.update_window().unwrap();
     }
 
-    fn update(&mut self) -> bool{
-        for event in self.event_pump.poll_iter() {
+    fn update(&mut self) -> bool {
+        for event in self.base.event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -89,20 +129,36 @@ impl ViewImage {
         }
 
         {
-            let mut win_surface = self.window.surface(&self.event_pump).unwrap();
+            let mut some_msg = None;
+            // empty every message from the channel, keep the latest
+            loop {
+                let possible_msg = self.base.rx.try_recv();
+                if let Ok(new_msg) = possible_msg {
+                    some_msg = Some(new_msg);
+                } else {
+                    break;
+                }
+            }
+            if let Some(mut msg) = some_msg {
+                self.msg_to_surface(&mut msg);
+            }
+
+            /*
             win_surface.fill_rect(
                 sdl2::rect::Rect::new(100, 100, 200, 300),
                 sdl2::pixels::Color::BLUE,
             ).unwrap();
             win_surface.update_window().unwrap();
+            */
         }
         true
     }
 }
 
 pub fn main() {
+    rosrust::init("view_image");
     let mut view_image = ViewImage::default(2000, 1000);
-    let mut msg = make_test_image(2000, 1000);
+    let mut msg = make_test_image(1000, 1000);
     view_image.msg_to_surface(&mut msg);
 
     'running: loop {
